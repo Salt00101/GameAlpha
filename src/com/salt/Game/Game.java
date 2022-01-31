b8513c6e9dcb4ea08c17b139ee556fb538883e72
anup <anupcpilla@gmail.com>
2022-01-31 14:37:22 -0500

    Initial Commit

--- src/com/salt/Game/Game.java
+++ src/com/salt/Game/Game.java
@@ -6,7 +6,5 @@ import com.salt.Game.Render.Window.Window;
 public class Game {
     public static void main(String[] args) throws InterruptedException {
         new Window().run();
-        // new Battle("Frog", 2, 1, true).battleScene();
-        System.out.println(Math.cos(69*Math.PI));
     }
 }
